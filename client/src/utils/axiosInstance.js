d3ad6854c1877a41440b6bb3e5ffc90b75b052ca
vaibhav shinde <vaibhu8605@gmail.com>
2025-11-06 14:48:01 +0530

    a

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://your-backend.onrender.com/api",
+    baseURL: "https://auth-server-k6ms.onrender.com/api",
     withCredentials: true,
 });
 
